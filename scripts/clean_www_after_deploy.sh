537553a53b39f66ce0fb9f7e958acb9549e5ce3f
Lane Englot <LaneEnglot@users.noreply.github.com>
2017-08-25 12:44:35 +0100

    update

--- scripts/clean_www_after_deploy.sh
+++ scripts/clean_www_after_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mv /var/www/html/src /tmp/web_src
-rm -fR /var/www/html
-mv /tmp/web_src /var/www/html 
-chmod -R 755 /var/www/html
+mv /usr/local/nginx/html/src /tmp/web_src
+rm -fR /usr/local/nginx/html
+mv /tmp/web_src /usr/local/nginx/html
+chmod -R 755 /usr/local/nginx/html
 rm -fR /tmp/web_src
