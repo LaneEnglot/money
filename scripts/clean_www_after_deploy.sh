01e75018e2bd4f426c423e318a4e6b4bcaa065f1
Lane Englot <LaneEnglot@users.noreply.github.com>
2017-08-26 12:47:45 +0100

    Update clean_www_after_deploy.sh

--- scripts/clean_www_after_deploy.sh
+++ scripts/clean_www_after_deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #backup new code b4 clear dir.
+if [ -d /tmp/web_src ]; then
+   rm -fR /tmp/web_src
+   mv /var/www/html/src /tmp/web_src
+fi
 mv /var/www/html/src /tmp/web_src
 
 # clear web root
-rm -fR /var/www/html
+rm -fR /var/www/html/*
 
 # if nginx config exists move it.
-if [ -f /tmp/web_src ]; then
+if [ -d /tmp/web_src ]; then
    mv /tmp/web_src /var/www/html
 fi
 
