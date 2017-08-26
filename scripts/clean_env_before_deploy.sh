4005e7af34eb5ebcc860488e2c6f1223f495eae5
Lane Englot <LaneEnglot@users.noreply.github.com>
2017-08-26 11:12:30 +0100

    Update clean_env_before_deploy.sh

--- scripts/clean_env_before_deploy.sh
+++ scripts/clean_env_before_deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# stop nginx if running
 isExistApp=`pgrep nginx`
 if [[ -n  $isExistApp ]]; then
-    systemctl stop nginx 
+    systemctl stop nginx
 fi
+# clear dir contents b4 code deploy extract...
 rm -fR /var/www/html
