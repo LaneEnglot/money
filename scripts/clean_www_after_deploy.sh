d4db6a79512245cd84d24d1dd59613b70c67661b
Lane Englot <LaneEnglot@users.noreply.github.com>
2017-08-26 11:04:34 +0100

    Update clean_www_after_deploy.sh

--- scripts/clean_www_after_deploy.sh
+++ scripts/clean_www_after_deploy.sh
@@ -2,6 +2,6 @@
 mv /var/www/html/src /tmp/web_src
 rm -fR /var/www/html
 mv /tmp/web_src /var/www/html
-mv /tmp/html_err_50x.ht /var/www/html/50x.html
+mv /tmp/nginx_error.php /var/www/html/error.php
 chmod -R 755 /var/www/html
 rm -fR /tmp/web_src
