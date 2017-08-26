6f65a9e17b765f8efefe9761e8d43cbd7f694aa7
Lane Englot <LaneEnglot@users.noreply.github.com>
2017-08-26 11:10:47 +0100

    Update clean_www_after_deploy.sh

--- scripts/clean_www_after_deploy.sh
+++ scripts/clean_www_after_deploy.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+#backup new code b4 clear dir.
 mv /var/www/html/src /tmp/web_src
+
+# clear web root
 rm -fR /var/www/html
-mv /tmp/web_src /var/www/html
-mv /tmp/nginx_error.php /var/www/html/error.php
+
+# if nginx config exists move it.
+if [ -f /tmp/web_src ]; then
+   mv /tmp/web_src /var/www/html
+fi
+
+# if error page exists move it.
+if [ -f /tmp/nginx_error.php ]; then
+   mv /tmp/nginx_error.php /var/www/html/error.php
+fi
 chmod -R 755 /var/www/html
-rm -fR /tmp/web_src
